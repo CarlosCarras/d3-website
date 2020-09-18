8ce54c7ca7989c2d58f7e37ae0073bf708e6de3b
Carlos Carrasquillo <36614712+CarlosCarras@users.noreply.github.com>
2020-09-18 12:23:57 -0400

    Delete config.js

--- server/config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    db : {
-        uri: 'mongodb://sh2020:gftew3cVXUsfLf4Av5EsCecJeDHynxYP@nas.jaxnb.net:26016/SwampHacks2020',
-    },
-    google: {
-        key: 'AIzaSyBP-SpyRI29xMrPDfdNZYAzgjMnVuDkyKc',
-    }
-};
